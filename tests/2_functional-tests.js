4a90e41c99cb0de831e918393656121ffda330da
jojosenthusiast <sweetenedbread@gmail.com>
2026-03-16 23:58:45 -0600

    solve functional test #5 - Zombie.js Colombo form

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -68,10 +68,16 @@ suite('Functional Tests', function () {
 
 const Browser = require('zombie');
 
+Browser.site = 'http://localhost:3000';
+
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  let browser;
 
-
+  suiteSetup(function(done) {
+    browser = new Browser();
+    browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -82,9 +88,15 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
